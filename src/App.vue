2adb4b2cbd765e7aea4b1a9feed2be785ab78efc
Júnior Moreira <junior@adunicamp.org.br>
2022-01-25 16:52:13 -0300

    Cria componente para mostrar senha

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app" class="container mx-auto mt-10 px-10">
+        <div class="mx-auto mt-20">
+            <h3 class="text-lg font-bold mb-4">Password Revealing</h3>
+
+            <label class="text-sm font-medium mb-2">Password</label>
+            <PasswordRevealing
+                :value="password"
+                placeholder="Digite sua senha"
+            />
+            <br />
+            {{ password }}
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PasswordRevealing from "./components/PasswordRevealing.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: "App",
+    components: { PasswordRevealing },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    data() {
+        return {
+            password: "",
+        };
+    },
+};
+</script>
