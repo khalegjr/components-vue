19984978e4e4a3eb44f2397200dd5a6d637dd61f
Júnior Moreira <junior@adunicamp.org.br>
2022-01-26 12:11:09 -0300

    Cria componente para itens do dropdown

---
+++ src/components/Dropdown/DropdownItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div
+        class="block flex items-center w-full px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 cursor-pointer"
+    >
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DropdownItem",
+};
+</script>
+
+<style></style>
