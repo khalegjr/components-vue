877cf72a1fbf2379cd93c0026e5b01750cc431a5
Júnior Moreira <junior@adunicamp.org.br>
2022-01-26 12:59:42 -0300

    Instala vue-router e cria rotas fake

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import VueRouter from "vue-router";
 
-createApp(App).mount("#app");
+const Foo = { template: "<div>foo</div>" };
+const Bar = { template: "<div>bar</div>" };
+
+const routes = [
+    { path: "/foo", name: "foo", component: Foo },
+    { path: "/bar", name: "bar", component: Bar },
+];
+
+const router = new VueRouter({
+    routes,
+});
+
+createApp(App).use(router).mount("#app");
